edf624734adfb9c1b87c73f3e5c618fa20ed8709
Michael Chinye <michaelchinye2018@gmail.com>
2022-08-03 16:29:58 +0100

    Initialized project

---
+++ src/utilities/Hashing.ts
@@ -0,0 +1,33 @@
+import bcrypt from 'bcrypt'
+export function encrypt (password: string){
+     bcrypt.genSalt(10, (err, salt) => {
+          if(err){
+               console.error(err)
+          }else{
+               bcrypt.hash(password, salt, (err, hash) => {
+                    if(err){
+                         console.error(err)
+                    }else{
+                         console.log(`this is the hash ${hash}`)
+                         return hash;
+                    }
+               })
+          }
+     })
+}
+
+export function compareHash (password:string, passwordHash: string){
+     bcrypt.compare(password, passwordHash, (err, res) => {
+          if(err){
+               console.error(err)
+          }else{
+               if(res){
+                    console.log("Password is correct")
+                    return true;
+               }else{
+                    console.warn('Password is incorrect')
+                    return false;
+               }
+          }
+     })
+}
